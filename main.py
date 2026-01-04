cf89fe36d2bcfd7a8044cdfed26529ce12cc6758
MacGoever <macgoever@gmx.li>
2026-01-04 13:17:54 +0100

    Fixed Cookoo timer
    Was @ XX:05, is now at XX:15

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ while True:
             ntp_success = helper.getTime()
             ntp_counter=42
 
-    if time[6] == 0 and time[5] in [0,5,30,45]:
+    if time[6] == 0 and time[5] in [0,15,30,45]:
     #if time[6] == 0 and True:
         lowerDot = color
         upperDot = color
